3ac74a6c6c771ab260c179e003aa875f23355ee5
fnaseer4 <youremail@who.com>
2016-10-27 17:51:32 +0500

    Material Color Pallete Setup

--- MainActivity.java
+++ MainActivity.java
@@ -2,13 +2,16 @@ package com.example.usmanyaqoob.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-//hello
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.appBar);
+        setSupportActionBar(toolbar);
     }
 }
